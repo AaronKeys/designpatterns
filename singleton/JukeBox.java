422546b4a1ee67c61aa8acda9e93bd183e3ff57f
AaronK <alkeys89@gmail.com>
2022-05-27 16:18:22 -0400

    comments and a minor change

--- singleton/JukeBox.java
+++ singleton/JukeBox.java
@@ -1,30 +1,40 @@
+/**
+ * @author Aaron L. Keys
+ */
 package singleton;
 
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.LinkedList;
 
+
 public class JukeBox {
     private static JukeBox jukeBox;
     private static ArrayList<Song> songs = DataLoader.getSongs();;
     private static Queue<String> songQueue = new LinkedList<String>();
     
+    /**
+     * creating a private constructor, so I can use a static instance
+     */
     private JukeBox() {}
 
+    /**
+     * This method alerts the user of creating a new jukebox instance.
+     * @return a static instance of the class, so I can call the jukebox
+     * as an instance.
+     */
     public static JukeBox getInstance() {
         if(jukeBox == null) {
             System.out.println("Creating a new jukebox");
             jukeBox = new JukeBox();
-            if(songs == null) {
-                System.out.println("Song's arraylist in jukebox is null");
-            }
-            for(Song song : songs) {
-                songQueue.add(song.toString());
-            }
         }
         return jukeBox;
     }
-
+    /**
+     * This method looks at the next song and returns null if empty,
+     * else it will return the next song in the queue.
+     * @return the next song in the queue.
+     */
     public String playNextSong() {
         if(songQueue.peek() == null) {
             return "You need to add songs to the list";
@@ -33,6 +43,14 @@ public class JukeBox {
         }
     }
 
+    /**
+     * This method returns a string, letting the user know
+     * the title of the song and the group who plays it. If
+     * the song isn't present in the list, the user is made
+     * aware of its absence.
+     * @param title is the name of the song
+     * @return the title of the song and the group who plays it.
+     */
     public String requestSong(String title) {
         int count = songQueue.size();
         for(Song song : songs) {
@@ -43,7 +61,12 @@ public class JukeBox {
         }
         return "Sorry we do not have the song " + title;
     }
-
+    /**
+     * This method allows the program to go through the queue until
+     * there is no longer a song in the queue.
+     * @return a boolean value that shows whether there is a song
+     * or not still in the queue.
+     */
     public boolean hasMoreSongs() {
         if(songQueue.peek() == null) {
             return false;
--- singleton/Song.java
+++ singleton/Song.java
@@ -1,14 +1,26 @@
+/**
+ * @author Aaron L. Keys
+ */
 package singleton;
 
 public class Song {
     private String title;
     private String artist;
-
+    
+    /**
+     * This constructor method takes the values given and
+     * gives those values to the appropriate variable name. 
+     * @param title is the name of the song's title.
+     * @param artist is the name of the song's artist.
+     */
     Song(String title, String artist) {
         this.title = title;
         this.artist = artist;
     }
-
+    /**
+     * This method let's the user know the name of the title and artist
+     * of the song that's being played.
+     */
     public String toString() {
         return title + " by " + artist;
     }
