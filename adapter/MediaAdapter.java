4c08440ed53e4f84b2930f247e5a8c8df49ce014
AaronK <alkeys89@gmail.com>
2022-06-16 17:22:54 -0400

    put the classes into vs code

---
+++ adapter/MediaAdapter.java
@@ -0,0 +1,34 @@
+package adapter;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MediaAdapter implements Media {
+    
+    private LittleMedia littleMedia;
+
+    public MediaAdapter(LittleMedia littleMedia) {
+
+    }
+
+    public String getTitle() {
+        return "";
+    }
+    
+    public Author getAuthor() {
+        Author a = new Author(, lastName)
+        return ;
+    }
+
+    public String getDescription() {
+        return "";   
+    }
+
+    public void addReview(String firstName, String lastName, Date date, double rating, String comment) {
+
+    }
+
+    public ArrayList<Review> getReviews() {
+        return ;
+    }
+}
