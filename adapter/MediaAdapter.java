d88397a9356c1537b1d4e6c0f096bee54288bae9
AaronK <alkeys89@gmail.com>
2022-06-16 19:09:35 -0400

    finished

--- adapter/MediaAdapter.java
+++ adapter/MediaAdapter.java
@@ -1,3 +1,6 @@
+/**
+ * @author Aaron L. Keys
+ */
 package adapter;
 
 import java.util.ArrayList;
@@ -5,17 +8,30 @@ import java.util.Date;
 import java.time.format.DateTimeFormatter;
 
 public class MediaAdapter implements Media {
-    
+    /**
+     * Declaring class variable.
+     */
     private LittleMedia littleMedia;
-
+    /**
+     * Constuctor method to pass the media upon use.
+     * @param littleMedia passes a type of media that
+     * will be used to convert to the newer system.
+     */
     public MediaAdapter(LittleMedia littleMedia) {
         this.littleMedia = littleMedia;
     }
-
+    /**
+     * Returning the title of the media (book)
+     * @return a String representing the title of the book.
+     */
     public String getTitle() {
         return littleMedia.getTitle();
     }
-    
+    /**
+     * Getting the author of the book.
+     * @return an Author class variable, which is
+     * the author of the book.
+     */
     public Author getAuthor() {
         String authorName = littleMedia.getAuthor();
         String[] fullName = authorName.split(" ", 2);
@@ -25,16 +41,30 @@ public class MediaAdapter implements Media {
 
         return author;
     }
-
+    /**
+     * Getting the description of the book.
+     * @return a String of the description of the book.
+     */
     public String getDescription() {
         return littleMedia.getDescription();   
     }
-
+    /**
+     * Adding a review to the LittleMedia class, which will then
+     * be used to add to the new system.
+     * @param firstName , which is the first name of the author of the review.
+     * @param lastName , which is the last name of the author of the review.
+     * @param date , which is the date that the comment was written.
+     * @param rating , is the rating of the book from the user.
+     * @param comment , a comment that someone made on the book.
+     */
     public void addReview(String firstName, String lastName, Date date, double rating, String comment) {
         Review review = new Review(firstName, lastName, date, rating, comment);
         littleMedia.addReview((firstName + " " + lastName), date, (int)Math.ceil(rating), comment);
     }
-
+    /**
+     * Getting the reviews of the book
+     * @return an ArrayList of Reviews
+     */
     public ArrayList<Review> getReviews() {
         ArrayList<String> reviewList = littleMedia.getReviews();
         ArrayList<Review> r = new ArrayList<Review>();
